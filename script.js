89d29970b5fc634a102a2dc423597f1c2f09ea2b
Moaad Amer <moaad02@gmail.com>
2023-09-24 17:20:06 +0300

    Make sure the next quote is different from the last one

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-let index = 0;
+
 const quotes = {
     "Jules Renard": `“The truly free man is the one who can turn down an invitation to dinner without giving an excuse.”`,
     "Albert Einstein": `“Our task must be to free ourselves... by widening our circle of compassion to embrace all living creatures and the whole of nature and it's beauty.”`,
     "C.S. Lewis": `“I was not born to be free---I was born to adore and obey.”`,
     "John Galsworthy": `“Life calls the tune, we dance.”`
 };
+let lastIndex = -1;
 function generate() {
 
     let authors = Object.keys(quotes);
-    let selectedAuthor = authors[Math.floor(Math.random() * authors.length)]
+
+    lastIndex = getNextIndex(lastIndex, authors.length);
+    console.log(lastIndex);
+
+    let selectedAuthor = authors[lastIndex];
 
     let selectedQuote = quotes[selectedAuthor];
 
     document.getElementById("quote").innerText = selectedQuote;
     document.getElementById("author").innerText = selectedAuthor;
 
+}
+
+// this function Insures the next index is different
+// from the lastIndex 
+
+function getNextIndex(lastIndex, length) {
+
+    let newIndex = Math.floor(Math.random() * length);
+    while (lastIndex == newIndex) {
+        newIndex = Math.floor(Math.random() * length);
+    }
+    return newIndex;
 }
